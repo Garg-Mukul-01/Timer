ea3a62cb650a42acea4c09e7507a3d5058258b3e
Garg-Mukul-01 <112013969+Garg-Mukul-01@users.noreply.github.com>
2022-10-08 12:22:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,29 +3,33 @@ let cntShowTimer = document.getElementById("show-timer-container");
 let minSpan = document.getElementById("mn");
 let colSpan = document.getElementById("colon");
 let secSpan = document.getElementById("sc");
-let sound = new Audio("tt1.mp3");
+let sound = new Audio("tt2.wav");
 cntShowTimer.style.display = "none";
 function start() {
   let min = +document.getElementById("min").value;
   let sec = +document.getElementById("sec").value;
-  if ((min >= 0 && min <= 60) && (sec >= 0 && sec <= 60)) {
+  if (min >= 0 && min <= 60 && sec >= 0 && sec <= 60) {
     cntTimer.style.display = "none";
     cntShowTimer.style.display = "inherit";
     let setTimer = setInterval(() => {
-      minSpan.innerText = min;
-      secSpan.innerText = sec;
+      if (min == 0) {
+        minSpan.style.display = "none";
+        colSpan.style.display = "none";
+      }
       if ((min > 0 && sec == 1) || (sec === 0 && min != 0)) {
         min--;
-        if(min==0){
-            minSpan.style.display="none";
-            colSpan.style.display="none";
-            console.log("testing");
+        if (min == 0) {
+          minSpan.style.display = "none";
+          colSpan.style.display = "none";
         }
         sec = 61;
         sec--;
       }
       if (sec > 0) sec--;
       else sound.play();
+      minSpan.innerText = min + "m";
+      colSpan.innerText = " : ";
+      secSpan.innerText = sec + "s";
     }, 1000);
   } else {
     alert("Invalid Inputs .");
